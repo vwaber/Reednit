579cf4747bf07b8dc2aa229d04aafc1b7d0f1c6c
vwaber <vwaber@gmail.com>
2018-03-20 13:51:02 -0400

    Begin implementation of Android Arch Components
    Unobtrusive changes

---
+++ app/src/main/java/com/reednit/android/arch/LinkViewModel.java
@@ -0,0 +1,27 @@
+package com.reednit.android.arch;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.reednit.android.room.Link;
+import com.reednit.android.room.Repository;
+
+import java.util.List;
+
+public class LinkViewModel extends AndroidViewModel {
+
+    private Repository mRepository;
+
+    private LiveData<List<Link>> mAllLinks;
+
+    public LinkViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new Repository(application);
+        mAllLinks = mRepository.getAllLinks();
+    }
+
+    LiveData<List<Link>> getAllLinks() { return  mAllLinks; }
+
+}
