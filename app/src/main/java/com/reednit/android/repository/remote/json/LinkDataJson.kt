1b42f4c09336ac65a82ef4088ac7f3f96a1c530e
vwaber <vwaber@gmail.com>
2018-04-20 14:34:39 -0400

    Remove unused code

--- app/src/main/java/com/reednit/android/repository/remote/json/LinkDataJson.kt
+++ app/src/main/java/com/reednit/android/repository/remote/json/LinkDataJson.kt
@@ -28,35 +28,3 @@ data class LinkDataJson(
         val selftextHtml: String
 
 )
-
-//package com.reednit.android.repository.remote.json;
-//
-//import com.squareup.moshi.Json;
-//
-//@SuppressWarnings("unused")
-//public class LinkDataJson {
-//
-//    @Json(name = "name")
-//    public String name;
-//    @Json(name = "subreddit")
-//    public String subreddit;
-//    @Json(name = "title")
-//    public String title;
-//    @Json(name = "thumbnail")
-//    public String thumbnail;
-//    @Json(name = "url")
-//    public String url;
-//    @Json(name = "author")
-//    public String author;
-//    @Json(name = "created_utc")
-//    public long createdUtc;
-//    @Json(name = "is_self")
-//    public boolean isSelf;
-//    @Json(name = "is_video")
-//    public boolean isVideo;
-//    @Json(name = "selftext")
-//    public String selftext;
-//    @Json(name = "selftext_html")
-//    public String selftextHtml;
-//
-//}
--- app/src/main/java/com/reednit/android/repository/remote/json/ListingJson.kt
+++ app/src/main/java/com/reednit/android/repository/remote/json/ListingJson.kt
@@ -6,28 +6,3 @@ data class ListingJson(
         @Json(name = "data")
         var data: ListingDataJson
 )
-
-//package com.reednit.android.repository.remote.json;
-//
-//import com.reednit.android.repository.local.Link;
-//import com.squareup.moshi.Json;
-//
-//import java.util.List;
-//
-//@SuppressWarnings("unused")
-//public class ListingJson {
-//
-//    @Json(name = "kind")
-//    private String kind;
-//    @Json(name = "data")
-//    private ListingDataJson data;
-//    @Json(name = "before")
-//    private String before;
-//    @Json(name = "after")
-//    private String after;
-//
-//    public List<Link> toModel(){
-//        return data.toModel();
-//    }
-//
-//}
